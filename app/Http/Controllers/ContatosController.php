17fef0ef61f34b216a86e9beb679719a6298be4f
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-16 15:56:19 -0300

    permissa

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -48,17 +48,16 @@ class ContatosController extends Controller
             $lead = DB::table('tb_atendimento as t1')
                 ->selectRaw("t1.at_id, t2.nome_do_produto, t1.at_nome_atendente, t1.at_id_responsavel, t1.at_inicio_atendimento, t1.at_final_atendimento, t2.id, t2.nome, t2.ddd,  t2.telefone, t2.email, t2.status, t2.insercao_hotmart, t2.obs_followup, t2.pos_atendimento, t2.id_responsavel")
                 ->join('tb_contatos as t2','t1.at_id_contato','=','t2.id')
-                ->whereRaw("t2.pos_atendimento = 'Vendido' AND t2.conferencia = 0 AND t1
-     .at_nome_atendente != 'Sistema' AND t1.at_id_responsavel = ".Auth::user()->id)
+                ->whereRaw("(t2.pos_atendimento = 'Vendido' AND t2.conferencia = 0 ) AND t1.at_nome_atendente != 'Sistema' AND t1.at_id_responsavel = ".Auth::user()->id)
                 ->orderBy('t1.at_final_atendimento','DESC')
                 ->get();
-        }elseif(Auth::user()->role == 3){
+            
+        }elseif(Auth::user()->role == 2){
 
             $lead = DB::table('tb_atendimento as t1')
                 ->selectRaw("t1.at_id, t2.nome_do_produto, t1.at_nome_atendente, t1.at_id_responsavel, t1.at_inicio_atendimento, t1.at_final_atendimento, t2.id, t2.nome, t2.ddd,  t2.telefone, t2.email, t2.status, t2.insercao_hotmart, t2.obs_followup, t2.pos_atendimento, t2.id_responsavel")
                 ->join('tb_contatos as t2','t1.at_id_contato','=','t2.id')
-                ->whereRaw("t2.pos_atendimento = 'Vendido' AND t2.conferencia = 0 AND t1
-     .at_nome_atendente != 'Sistema' AND t1.at_id_responsavel = ".Auth::user()->id)
+                ->whereRaw("(t2.pos_atendimento = 'Vendido' AND t2.conferencia = 0 ) AND t1.at_nome_atendente != 'Sistema' AND t2.id_responsavel =  ".Auth::user()->id)
                 ->orderBy('t1.at_final_atendimento','DESC')
                 ->get();
         }
