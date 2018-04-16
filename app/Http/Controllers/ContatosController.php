085c1e8ebbb0d26221a6f3de4dcc10c68948d2a8
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-16 18:26:24 -0300

    outra permissao 4

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -174,22 +174,30 @@ class ContatosController extends Controller
     }
 
     public function nao_atendidos(){
-        if(Auth::user()->role == 1){
+        if(Auth::user()->role == 1 || Auth::user()->role == 4){
         $lead = DB::table('tb_atendimento as t1')
             ->selectRaw("t1.at_id, t1.at_nome_atendente, t1.at_inicio_atendimento, t1.at_final_atendimento, t2.id, t2.ddd, t2.nome, t2.telefone, t2.id_responsavel, t2.email, t2.status, t2.obs_followup, t2.insercao_hotmart, t2.pos_atendimento")
             ->join('tb_contatos as t2','t1.at_id_contato','=','t2.id')
             ->whereRaw("t2.pos_atendimento IN('Boleto NÃ£o Atendido', 'NÃ£o Atendeu') OR t2.pos_atendimento IN('Boleto Não Atendido', 'Não Atendeu')")
             ->orderBy('t1.at_final_atendimento','DESC')
             ->get();
-        }else {
+
+        }elseif(Auth::user()->role >= 3) {
+                $lead = DB::table('tb_atendimento as t1')
+                    ->selectRaw("t1.at_id, t1.at_nome_atendente, t1.at_inicio_atendimento, t1.at_final_atendimento, t2.id, t2.ddd, t2.nome, t2.telefone, t2.id_responsavel, t2.email, t2.status, t2.obs_followup, t2.insercao_hotmart, t2.pos_atendimento")
+                    ->join('tb_contatos as t2', 't1.at_id_contato', '=', 't2.id')
+                    ->whereRaw("t2.pos_atendimento IN ('Boleto Não Atendido', 'Não Atendeu') AND t1.at_id_responsavel = " . Auth::user()->id)
+                    ->orderBy('t1.at_final_atendimento', 'DESC')
+                    ->get();
+
+        }elseif (Auth::user()->role == 2){
             $lead = DB::table('tb_atendimento as t1')
                 ->selectRaw("t1.at_id, t1.at_nome_atendente, t1.at_inicio_atendimento, t1.at_final_atendimento, t2.id, t2.ddd, t2.nome, t2.telefone, t2.id_responsavel, t2.email, t2.status, t2.obs_followup, t2.insercao_hotmart, t2.pos_atendimento")
-                ->join('tb_contatos as t2','t1.at_id_contato','=','t2.id')
-                ->whereRaw("t2.pos_atendimento IN('Boleto NÃ£o Atendido', 'NÃ£o Atendeu') OR t2.pos_atendimento IN('Boleto Não Atendido', 'Não Atendeu') AND t1.at_id_responsavel = ".Auth::user()->id)
-                ->orderBy('t1.at_final_atendimento','DESC')
+                ->join('tb_contatos as t2', 't1.at_id_contato', '=', 't2.id')
+                ->whereRaw("t2.pos_atendimento IN ('Boleto Não Atendido', 'Não Atendeu') AND t2.id_responsavel = " . Auth::user()->id)
+                ->orderBy('t1.at_final_atendimento', 'DESC')
                 ->get();
         }
-
         return view('contatos.leads.nao-atendidos', ['contatos' => $lead]);
     }
 
