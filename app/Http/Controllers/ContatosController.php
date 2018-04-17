19c2ffd0681c63b8e3a135be664644a503a69349
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-17 14:27:20 -0300

    contatos ligar depois

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -134,7 +134,8 @@ class ContatosController extends Controller
         $lead = DB::table('tb_atendimento as t1')
             ->selectRaw("t1.at_id, t1.at_nome_atendente, t1.at_inicio_atendimento, t1.at_final_atendimento, t1.at_data_ligar_depois, t2.id, t2.nome, t2.telefone, t2.email, t2.observacao, t2.id_responsavel, t2.obs_followup, t2.status, t2.insercao_hotmart, t2.ddd, t2.obs_followup, t2.pos_atendimento, t2.data_ligar_depois")
             ->join('tb_contatos as t2','t1.at_id_contato','=','t2.id')
-            ->whereRaw("t2.pos_atendimento = 'Ligar Depois' AND t1.at_nome_atendente != 'Sistema'")
+            ->where("t2.pos_atendimento", '=',"Ligar Depois")
+            ->where('t1.at_nome_atendente', '!=', 'Sistema')
             ->orderBy('data_ligar_depois','ASC')
             ->get();
 
