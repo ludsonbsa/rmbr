b0834fa7bb69751e00f21e5a4235c2a459a1009b
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-16 21:07:22 -0300

    arrumando os bug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <!--<div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
@@ -48,7 +48,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div>-->
 
                         <div class="form-group">
                             <div class="col-md-12">
@@ -56,9 +56,10 @@
                                     Login
                                 </button>
                             </div>
-                                <a class="btn btn-link col-md-6 col-md-offset-3" href="{{ route('password.request') }}">
+                               <!-- <a class="btn btn-link col-md-6 col-md-offset-3" href="{{ route('password.request')
+                               }}">
                                     {{trans('auth.forgot')}}
-                                </a>
+                                </a>-->
                             </div>
                     </form>
                         </div>
