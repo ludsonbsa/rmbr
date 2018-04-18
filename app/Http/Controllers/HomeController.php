5e042544224c657028047feb288a37f80fa830cf
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-18 12:01:10 -0300

    Post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,16 @@ class HomeController extends Controller
 
     public function infusion(Request $request)
     {
-        
-        var_dump($request);
+
+        if($request['inf_field_FirstName']){
+            $caminho = public_path().'/uploads/planilhas';
+            $fp = fopen($caminho, "a");
+
+            // Escreve "exemplo de escrita" no bloco1.txt
+            $escreve = fwrite($fp, $request['inf_field_FirstName']);
+
+            // Fecha o arquivo
+            fclose($fp);
+        }
     }
 }
