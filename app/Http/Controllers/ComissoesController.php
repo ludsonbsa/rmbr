6e7fcbee9f8f941213347f736fc072c1d75e3ea7
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-18 09:50:16 -0300

    otimizado comissao

--- app/Http/Controllers/ComissoesController.php
+++ app/Http/Controllers/ComissoesController.php
@@ -12,23 +12,23 @@ class ComissoesController extends Controller
     public function conferencia()
     {
         //listagem das comissões a serem conferidas
-        /*$query = DB::table('tb_contatos as t1')
+        $query = DB::table('tb_contatos as t1')
             ->selectRaw("t1.id, t1.documento_usuario, t1.nome, t1.email, t1.telefone, t1.insercao_hotmart, t1.ddd, t2.user_nome")
             ->join('users as t2','t1.id_responsavel','=','t2.id')
             ->whereIn('t1.pos_atendimento', ['Vendido', 'Boleto Gerado'])
             ->where('t1.conferencia','=', 0)
             ->groupBy('t1.email')
             ->orderBy('t1.id','ASC')
-            ->paginate(25);*/
+            ->get();
 
-        $query = DB::table('tb_atendimento as t1')
+        /*$query = DB::table('tb_atendimento as t1')
             ->selectRaw("t1.at_id, t2.nome_do_produto, t1.at_nome_atendente, t1.at_inicio_atendimento, t1.at_final_atendimento, t2.id, t2.nome, t2.ddd, t2.telefone, t2.email, t2.status, t2.insercao_hotmart, t2.pos_atendimento, t2.id_responsavel")
             ->join('tb_contatos as t2','t1.at_id_contato','=','t2.id')
             ->whereIn('t2.pos_atendimento',['Vendido','Boleto Gerado'])
             ->where('t2.conferencia' , '=', 0)
             ->where('t1.at_nome_atendente', '!=', 'Sistema')
             ->orderBy('t1.at_final_atendimento','DESC')
-            ->get();
+            ->get();*/
 
         $count = $query->count();
 
