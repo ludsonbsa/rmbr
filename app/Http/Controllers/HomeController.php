96bd7618dad4619680a91c9440b1391ea8358f97
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-18 16:41:18 -0300

    Vai

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,18 @@ class HomeController extends Controller
     public function infusion(Request $request)
     {
 
-        $param = $request->input('inf_field_FirstName');
+        $param =
+              $request->input('inf_field_FirstName').'\n'
+            . $request->input('inf_field_Email').'\n'
+            . $request->input('inf_field_SSN').'\n'
+            . $request->input('inf_custom_DDDTelefone').'\n'
+            . $request->input('inf_custom_EstadoSigla').'\n'
+              . $request->input('inf_field_Address2Street1').'\n'
+              . $request->input('inf_field_Address2Street2').'\n'
+              . $request->input('inf_field_City2').'\n'
+              . $request->input('inf_field_PostalCode2').'\n'
+              . $request->input('inf_custom_Numero').'\n'
+              . $request->input('inf_custom_Complemento');
 
         $caminho = public_path().'/uploads/planilhas/teste.txt';
         $fp = fopen($caminho, "a");
