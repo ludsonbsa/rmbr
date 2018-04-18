ed37352b97ec6e23fc7cf2c99e81629b2037cc9f
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-18 15:28:17 -0300

    Vai

--- routes/web.php
+++ routes/web.php
@@ -175,7 +175,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
     /****************************************SUPORTE********************************************/
 
     /*******EXTERNO***********/
-    Route::put('infusion/', 'HomeController@infusion')->name('infusion');
+    Route::any('infusion/', 'HomeController@infusion')->name('infusion');
 
     Route::get('form/', 'HomeController@form')->name('infusion.form');
 
