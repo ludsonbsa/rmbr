89df0f9929c5aa9ccfc688434880cfe5f2c2f064
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-28 11:48:59 -0300

    BORA

--- resources/views/comissoes/comissionar-pendentes.blade.php
+++ resources/views/comissoes/comissionar-pendentes.blade.php
@@ -35,7 +35,6 @@
     </tbody>
 
 </table>
-    <input type="hidden" name="nome_do_produto" value="{!! $contato->nome_do_produto !!}">
 <?php
 if($contagem == 0){
     echo "<h3 style=\"font-size:18px; float:left; font-weight: 500; color:#636363; margin-left:18px; margin-top:18px; padding:17px;\">Nenhuma venda pendente</h3>";
@@ -43,7 +42,7 @@ if($contagem == 0){
     ?>
     <h3 style="font-size:18px; float:left; font-weight: 500; color:#636363; margin-top:8px; padding:17px;">Você precisa comissionar {{$contagem}} venda(s)</h3>
     <div class="field-wrap" style="float: right;">
-        
+        <a href="{{route('admin.comissoes.relatorio-pendente')}}" class="enviar"><button class="enviar">Gerar PDF</button></a> &nbsp;
         <button class="enviar" type="submit">Comissionar Vendas</button>
     </div>
 
