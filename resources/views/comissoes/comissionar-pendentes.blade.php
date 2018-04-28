3ca31467a91fb9d8b0fa068e5bc115ab3ac33c1d
Ludson Almeida <ludson.bsa@gmail.com>
2018-04-28 11:45:50 -0300

    'pdf'

--- resources/views/comissoes/comissionar-pendentes.blade.php
+++ resources/views/comissoes/comissionar-pendentes.blade.php
@@ -13,7 +13,24 @@
     </thead>
     <tbody>
 
-       
+        @foreach($contatos as $contato)
+
+        <tr class="odd">
+
+            <td class="nome">{!! $contato->nome !!}</td>
+            <input type="hidden" name="nome" value="{!! $contato->nome !!}">
+            <td>{!! $contato->documento_usuario !!}</td>
+            <input type="hidden" name="documento_usuario" value="{!! $contato->documento_usuario !!}">
+            <td>{!! $contato->telefone !!}</td>
+            <input type="hidden" name="telefone" value="{!! $contato->telefone !!}">
+            <td>{!! $contato->data_de_venda !!}</td>
+            <td>{!! $contato->nome_do_produto !!}</td>
+            <td>{!! $contato->email !!}</td>
+            <input type="hidden" name="email" value="{!! $contato->email !!}">
+
+
+        </tr>
+        @endforeach
 
     </tbody>
 
@@ -26,8 +43,7 @@ if($contagem == 0){
     ?>
     <h3 style="font-size:18px; float:left; font-weight: 500; color:#636363; margin-top:8px; padding:17px;">Você precisa comissionar {{$contagem}} venda(s)</h3>
     <div class="field-wrap" style="float: right;">
-
-        <a href="{{route('admin.comissoes.relatorio-pendente')}}" class="enviar"><button class="enviar">Gerar PDF</button></a>
+        
         <button class="enviar" type="submit">Comissionar Vendas</button>
     </div>
 
